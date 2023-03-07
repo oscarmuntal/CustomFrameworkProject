1072fc38ff4717038ad0ce1dde4777eafe34701b
Òscar Muntal <oscar.muntal@gmail.com>
2023-03-07 13:23:47 +0100

    Initial Commit

---
+++ CustomFrameworkProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomFrameworkProject
+//
+//  Created by Òscar Muntal on 7/3/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomFrameworkProject/CustomFrameworkProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomFrameworkProjectApp.swift
+//  CustomFrameworkProject
+//
+//  Created by Òscar Muntal on 7/3/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomFrameworkProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
